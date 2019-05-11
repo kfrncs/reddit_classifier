78b07f814f1053bc65f16436137c80363a8bf5f2
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-11 17:58:22 -0400

    add category, make variable i more descriptive, comments

--- post_scraper.py
+++ post_scraper.py
@@ -24,35 +24,42 @@ headers = {"Authorization": REDDIT_ID + REDDIT_TOKEN, "User-Agent": "reddit_scra
 
 class Scraper:
 
-    def __init__(self, subreddit='conspiracytheories'):
+    def __init__(self, subreddit='conspiracytheories', category='unknown'):
         ''' A class to scrape Reddit posts for bag-of-words NLP'''
 
         # "after" attribute for tracking how far we have scraped into the sub's history
         self.after = None
         # set subreddit to scrape
         self.subreddit = subreddit
-        # empty attributes
+        # empty content list to append titles/post content
         self.content = []
+
+        # category must be set for the corpus
+        if category == 'unknown':
+            self.category = input(f'Please input the category for {self}')
+        else:
+            self.category = category
         
 
     def scrape(self, pages=5):
         """ fetch posts from Reddit API. Iterations chooses how many pages to scrape. """
 
-        for i in range(1, pages+1):
+        for page in range(1, pages+1):
             # if self.after exists, start from that post
             if self.after:
                 self.last_request = requests.get(f'https://www.reddit.com/r/{self.subreddit}.json?after={self.after}',
                                             headers=headers)
-                print(f'fetched page {i}')
+                print(f'fetched page {page}')
 
             # else start at the beginning
             else:
                 self.last_request = requests.get(f'https://www.reddit.com/r/{self.subreddit}.json',
                                             headers=headers)
-                print(f'fetched page {i}')
+                print(f'fetched page {page}')
 
             # parse JSON from the last request, inplace
             self.last_request = json.loads(self.last_request.text)
+            print(f'Parsed page {page} as JSON')
 
             # iterate through posts in most recently fetched JSON
             for post in range(len(self.last_request['data']['children'])):
@@ -60,12 +67,12 @@ class Scraper:
                 # If post has a "selftext" field, append it to the content list
                 if self.last_request['data']['children'][post]['data']['selftext']:    
                     self.content.append(self.last_request['data']['children'][post]['data']['selftext'])
-                    print(f'appended selftext from post {post}')
+                    print(f'appended selftext from post {post}, page {page} to content')
 
                 # If post has "title", append it to content list
                 elif self.last_request['data']['children'][post]['data']['title']:
                     self.content.append(self.last_request['data']['children'][post]['data']['title'])
-                    print(f'appended title from post {post}')
+                    print(f'appended title from post {post}, page {page} to content')
 
 
     #   if 'selftext':
