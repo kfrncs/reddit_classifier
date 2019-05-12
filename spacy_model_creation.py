c6bf83c40c604a42c49fbb66e286ea0efaa7a0a5
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-12 19:08:56 -0400

    make knn, cnd, logreg models

--- spacy_model_creation.py
+++ spacy_model_creation.py
@@ -10,8 +10,13 @@ import string
 import pandas as pd
 import spacy
 from spacy.lang.en import English
+
+# sklearn imports
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import ComplementNB
 
 # load spacy model, set the stopwords
 print('loading spacy en_core_web_sm')
@@ -29,23 +34,24 @@ def spacy_tokenizer(post):
     
         adapted from https://www.dataquest.io/blog/tutorial-text-classification-in-python-using-spacy/
     """
+
     my_tokens = parser(post)
-    
+
     # -PRON- bit is for pronouns: https://spacy.io/api/annotation#lemmatization
     my_tokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in my_tokens ]
 
     my_tokens = [ word for word in my_tokens if word not in spacy_stopwords and word not in punctuation ]
-    
+
     return ' '.join(my_tokens)
 
 def df_to_list(df):
-    """ Take a DataFrame, return a list with all the values from 'document' """
+    """ Take input DataFrame, return a list with all the values from 'document' """
     df_list = list(df['document'].values)
     return df_list
 
 
 if __name__ == "__main__":
-    
+
     td_df = pd.read_csv('data/td-2019-05-11.csv')
     print('loaded td_df')
     ct_df = pd.read_csv('data/ct-2019-05-11.csv')
@@ -56,9 +62,9 @@ if __name__ == "__main__":
     ct_list = df_to_list(ct_df)
     print('converted ct_df to ct_list')
 
-    td_list = td_list[:1000]
+    td_list = td_list[:12000]
     print('shortened td_list')
-    ct_list = ct_list[:1000]
+    ct_list = ct_list[:12000]
     print('shortened ct_list')
     
     print('starting Spacy tokenization on td_list')
@@ -86,13 +92,33 @@ if __name__ == "__main__":
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
     print('train_test_split')
 
-    
+    # Vectorize
+    vectorizer = CountVectorizer(max_features=5000)     
+    train_data_features = vectorizer.fit_transform(x_train)
+    test_data_features = vectorizer.transform(x_test)
+    vocab = vectorizer.get_feature_names()
+    print('trained and transformed w/ vectorizer')
 
+    # model training
+    log_reg = LogisticRegression()
+    log_reg.fit(train_data_features, y_train)
+    lr_preds = log_reg.predict(test_data_features) 
 
-    # do we need these?
-    # tvec = TfidfVectorizer(stop_words='english')
-    # tvec.fit(corpus)
-    # corpus_matrix = tvec.transform(corpus)
+    knn = KNeighborsClassifier()
+    knn.fit(train_data_features, y_train)
+    knn_preds = knn.predict(test_data_features) 
 
+    cnb = ComplementNB()
+    cnb.fit(train_data_features, y_train)
+    cnb_preds = cnb.predict(test_data_features) 
 
+    # make df with all preds
+    df = pd.DataFrame(list(zip(cnb_preds, lr_preds, knn_preds, y_test, x_test)), 
+                      columns=['cnb_preds', 'lr_preds', 'knn_preds', 'category', 'document'])
+
+    lr_incorrect = df[df['lr_preds'] != df['category']]
+    knn_incorrect = df[df['knn_preds'] != df['category']]
+    cnb_incorrect = df[df['cnb_preds'] != df['category']]
+    two_incorrect = knn_incorrect[knn_incorrect['lr_preds'] != knn_incorrect['category']]
+    all_incorrect = two_incorrect[two_incorrect['cnb_preds'] != two_incorrect['category']]
 
