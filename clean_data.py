d24d0f0d697ab995dfd3fb454a7f2b31117caf58
Ken Francis <kennethpatrickfrancis@gmail.com>
2019-05-11 22:58:11 -0400

    change df_to_str to df_to_list, remove nltk

--- clean_data.py
+++ clean_data.py
@@ -5,20 +5,36 @@ Data Cleaning
 
 # imports
 import pandas as pd
-from nltk.tokenize import RegexpTokenizer
 
-def df_to_str(df):
-    """ Take a DataFrame, return a string with all the values in 'document' joined together """
+def df_to_list(df):
+    """ Take a DataFrame, return a list with all the values from 'document' """
     df_list = list(df['document'].values)
-    df_str = ' '.join(df_list)
-    return df_str
+    return df_list
+
+def drop_punctuation(in_list):
+    """ drop all the punctuation from a list of strings"""
+    out_list = in_list.copy()
+
+    for i in range(len(out_list)):
+        out_list[i] = out_list[i].replace('.', '').replace(',', '').replace('\'', '').replace('"', '')
+        out_list[i] = out_list[i].replace('/', '').replace('\\', '').replace('!', '').replace('‘', '')
+        out_list[i] = out_list[i].replace('“', '').replace('(', '').replace(')', '').replace('”', '')
+        out_list[i] = out_list[i].replace('?', '')
+        out_list[i] = out_list[i].replace(':', '').replace('’', '').lower()
+
+    return out_list
 
 td_df = pd.read_csv('data/td-2019-05-11.csv')
 ct_df = pd.read_csv('data/ct-2019-05-11.csv')
 
-td_str = df_to_str(td_df)
-ct_str = df_to_str(ct_df)
+td_list = df_to_list(td_df)
+ct_list = df_to_list(ct_df)
 
-tokenizer = RegexpTokenizer(r'\w+')
+td_list = drop_punctuation(td_list)
+ct_list = drop_punctuation(ct_list)
+
+td_list = td_list[:12000]
+ct_list = td_list[:12000]
+
 
 
